e968d42feb4f3682c0fbdc122f7b5cbeb07c8533
Marcel Reig <marcelreig@gmail.com>
2025-07-14 18:47:08 +0000

    add people and  planets endpoints

--- src/app.py
+++ src/app.py
@@ -8,15 +8,19 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import select
+
+from models import db, User, Planet, Character
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -27,14 +31,19 @@ CORS(app)
 setup_admin(app)
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -44,6 +53,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/people', methods=['GET'])
+def get_characters():
+    all_characters = db.session.execute(select(Character)).scalars().all()
+    results = list(
+        map(lambda character: character.serialize(), all_characters))
+    return jsonify(results), 200
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_character_by_id(people_id):
+    character = db.session.get(Character, people_id)
+    if character is None:
+        return jsonify({"msg": "Character not found"}), 404
+
+    return jsonify(character.serialize()), 200
+
+
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    planets = db.session.execute(select(Planet)).scalars().all()
+    results = list(map(lambda planet: planet.serialize(), planets))
+    return jsonify(results), 200
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet_by_id(planet_id):
+    planet = db.session.get(Planet, planet_id)
+    if planet is None:
+        return jsonify({"msg": "Planet not found"}), 404
+
+    return jsonify(planet.serialize()), 200
+
+
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey, Integer, BigInteger, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+import enum
 
 db = SQLAlchemy()
 
@@ -16,4 +17,50 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Planet(db.Model):
+    __tablename__ = "planets"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120), nullable=False)
+    clima: Mapped[str] = mapped_column(String(120))
+    terreno: Mapped[str] = mapped_column(String(120))
+    poblacion: Mapped[int] = mapped_column(BigInteger)
+
+    # characters: Mapped[list["Character"]] = relationship(
+    #     "Character", back_populates="planeta_origen")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "clima": self.clima,
+            "terreno": self.terreno,
+            "poblacion": self.poblacion,
+        }
+
+class Character(db.Model):
+    __tablename__ = "characters"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120), nullable=False)
+    genero: Mapped[str] = mapped_column(
+        Enum("male", "female", "n/a", "unknown", name="genero_enum"), nullable=False)
+    raza: Mapped[str] = mapped_column(String(80))
+
+  
+    # planeta_origen_id: Mapped[int] = mapped_column(ForeignKey("planets.id"))
+
+    # planeta_origen: Mapped["Planet"] = relationship(
+    #     "Planet", back_populates="characters")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "genero": self.genero,
+            "raza": self.raza,
+            # "planeta_origen_id": self.planeta_origen_id,
         }
